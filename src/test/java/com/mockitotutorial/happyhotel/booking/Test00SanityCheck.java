84c5a80eade7ed62be2b390d6631237ce21cb5a9
DamianArado <khanhumayun95@gmail.com>
2021-05-17 17:58:58 +0530

    Add files✈

---
+++ src/test/java/com/mockitotutorial/happyhotel/booking/Test00SanityCheck.java
@@ -0,0 +1,14 @@
+// Mockito: Next-Level Java Unit Testing by Adrian Więch
+
+package com.mockitotutorial.happyhotel.booking;
+
+import org.junit.jupiter.api.Test;
+
+class Test00SanityCheck {
+
+	@Test
+	void test() {
+		// this should pass
+	}
+
+}
